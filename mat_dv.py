e0cffd7820474562b88f72790f87875a2bdca7d4
egorkin18 <egor.boyko.69@mail.ru>
2021-02-15 18:00:58 +0600

    pishu sql

--- mat_dv.py
+++ mat_dv.py
@@ -1,32 +1,9 @@
-with open('mat_dv.txt','r') as f:
-    k1=0
-    k2=0
-    k3=0
-    k4=0
-    max=0
-    for i in f:
-        a=i.split()
-        print(a)
-
-        if a[2]=='8':
-            k1=int(a[3])+int(a[4])
-
-        if a[2]=='9':
-            k2=a[3]+a[4]
-
-        if a[2]=='10':
-            k3=a[3]+a[4]
-
-        if a[2]=='11':
-            k4=int(a[3]+a[4])
-
-
-
-    print('8 классы: ',k1)
-    print('9 классы: ',k2)
-    print('10 классы: ',k3)
-    print('11 классы: ',k4)
-
-
+from random import randint
+l=list()
+for i in range(0,10):
+    l.append(randint(1,15))
+    while l.count(l[i])>0:
+        l[i]=randint(1,15)
+print(l)
 
 
